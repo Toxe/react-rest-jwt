52697f87d1239a99c5efe9d4bce2c3e882d13e97
Toxe <toxe@toxe.net>
2020-05-18 20:08:34 +0200

    Show server message in request error

--- src/RequestError.js
+++ src/RequestError.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function RequestError({ error }) {
-    return <div className="RequestError">Request Error: {error.message}</div>;
+    return (
+        <div className="RequestError">
+            Request error: {error.message}<br />
+            Server response: {error.response.status} {error.response.data.error}
+            {error.response.data.message && (
+                <>
+                    <br />"{error.response.data.message}"
+                </>
+            )}
+        </div>
+    );
 }
