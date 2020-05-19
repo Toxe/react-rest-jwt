718859ea023b3a89e831d9a80e89f5cfb22a2395
Toxe <toxe@toxe.net>
2020-05-19 17:50:20 +0200

    Show messages from server response

--- src/RequestError.js
+++ src/RequestError.js
@@ -5,11 +5,7 @@ export default function RequestError({ error }) {
         <div className="RequestError">
             Request error: {error.message}<br />
             Server response: {error.response.status} {error.response.data.error}
-            {error.response.data.message && (
-                <>
-                    <br />"{error.response.data.message}"
-                </>
-            )}
+            {error.response.data.message && (<pre>{JSON.stringify(error.response.data.message, null, "  ")}</pre>)}
         </div>
     );
 }
