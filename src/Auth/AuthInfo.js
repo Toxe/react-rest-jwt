cb7e27d69c169971cabe9fe28be037a8e5d2a702
Toxe <toxe@toxe.net>
2020-05-23 12:53:28 +0200

    Show identity value from token

--- src/Auth/AuthInfo.js
+++ src/Auth/AuthInfo.js
@@ -3,12 +3,9 @@ import React from "react";
 export default function AuthInfo({ auth }) {
     return (
         <div className="AuthInfo">
-            <div>
-                <strong>access_token:</strong> {auth.access_token}
-            </div>
-            <div>
-                <strong>refresh_token:</strong> {auth.refresh_token}
-            </div>
+            <div><strong>access_token:</strong> {auth.access_token}</div>
+            <div><strong>refresh_token:</strong> {auth.refresh_token}</div>
+            <div><strong>identity:</strong> {auth.userId}</div>
         </div>
     );
 }
