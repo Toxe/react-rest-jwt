61f1530810249425e3d169f73917a8849f75e6d1
Toxe <toxe@toxe.net>
2020-05-23 12:52:09 +0200

    Decode tokens and store user id

--- src/Auth/AuthHeader.js
+++ src/Auth/AuthHeader.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
+import jwt from "jsonwebtoken";
 import "./Auth.css";
 import LoginSection from "./LoginSection";
 import LogoutSection from "./LogoutSection";
 import AuthInfo from "./AuthInfo";
 import RequestError from "../RequestError";
+import { TokenDecodeError } from "../Errors";
 
 export default function AuthHeader() {
     const [auth, setAuth] = useState(null);
@@ -14,9 +16,19 @@ export default function AuthHeader() {
         axios
             .post("/auth/login", credentials)
             .then((res) => {
-                setAuth(res.data);
-                setRequestError(null);
-                axios.defaults.headers["Authorization"] = `Bearer ${res.data.access_token}`;
+                try {
+                    const access_token_data = jwt.decode(res.data.access_token);
+                    const refresh_token_data = jwt.decode(res.data.refresh_token);
+
+                    if (access_token_data === null || refresh_token_data === null)
+                        throw new TokenDecodeError("Unable to decode token");
+
+                    setAuth({ ...res.data, userId: access_token_data.identity });
+                    setRequestError(null);
+                    axios.defaults.headers["Authorization"] = `Bearer ${res.data.access_token}`;
+                } catch (err) {
+                    throw new TokenDecodeError("Unable to decode token");
+                }
             })
             .catch((error) =>
                 setRequestError(<RequestError error={error} handleClose={() => setRequestError(null)} />)
