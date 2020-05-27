a6f6079e74ca89c393f6af85b48bceb3433ebe9a
Toxe <toxe@toxe.net>
2020-05-27 16:05:56 +0200

    Queue parallel requests when refreshing the access token

--- src/Auth/ResponseInterceptor.js
+++ src/Auth/ResponseInterceptor.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 let installedResponseInterceptor = null;
+let isRefreshing = false;
+let pendingRequestsQueue = [];
 
 export function addResponseInterceptor(refresh) {
     installedResponseInterceptor = axios.interceptors.response.use(
@@ -19,13 +21,46 @@ async function errorResponseInterceptor(error, refresh) {
     const origResponse = error.response;
 
     if (origResponse.status === 401 && origResponse.data.error === "Token has expired" && !origRequest._retry) {
+        if (isRefreshing) {
+            // already refreshing, queue request
+            pendingRequestsQueue.push(origRequest);
+            return null;
+        }
+
+        isRefreshing = true;
+
         const access_token = await refresh();
 
         origRequest._retry = true;
         origRequest.headers["Authorization"] = `Bearer ${access_token}`;
 
-        return await axios(origRequest);
+        // resend original request
+        await axios(origRequest);
+
+        isRefreshing = false;
+
+        // resend all pending requests
+        return await resendPendingRequests(access_token);
     }
 
     return error;
+}
+
+async function resendPendingRequests(access_token) {
+    if (pendingRequestsQueue.length > 0) {
+        // prepare requests
+        const requests = pendingRequestsQueue.map((config) => {
+            config._retry = true;
+            config.headers["Authorization"] = `Bearer ${access_token}`;
+            return axios(config);
+        });
+
+        try {
+            // clear queue and resend all pending requests with new access token
+            pendingRequestsQueue = [];
+            return await axios.all(requests);
+        } catch (e) {
+            return e;
+        }
+    }
 }
