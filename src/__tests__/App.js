8d182750064e2c8c1aa49f3ae6ec3afc4cab284b
Toxe <toxe@toxe.net>
2020-06-23 16:47:21 +0200

    Update App test

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import axiosMock from "axios";
 import App from "../App";
 
-it("renders", () => {
-    render(<App />);
+jest.mock("axios");
+
+it("renders", async () => {
+    const { getAllByText } = render(<App />);
+
+    await waitFor(() => expect(axiosMock.get).toHaveBeenCalledTimes(2));
+    expect(axiosMock.get).toHaveBeenCalledWith("/api/users");
+    expect(axiosMock.get).toHaveBeenCalledWith("/api/ships");
+
+    getAllByText("Login");
+    getAllByText("Users");
+    getAllByText("Ships");
 });
